89847a1d200503a1dfe159b8d385d2b6829bcf7d
Felipe Daragon <felipe@syhunt.com>
2014-02-26 18:47:56 -0300

    Add check for presence of parsebody function
    This makes Sailor run in Apache mod_lua 2.4.2 or lower

--- src/sailor.lua
+++ src/sailor.lua
@@ -12,8 +12,11 @@ local Page = {}
 function sailor.init(r,p)
     sailor.path = p
 
-    local POST, POSTMULTI = r:parsebody()
     local GET, GETMULTI = r:parseargs()
+    local POST, POSTMULTI = {}, {}
+    if r.parsebody ~= nil then -- only present in Apache 2.4.3 or higher
+        POST, POSTMULTI = r:parsebody()
+    end
     local page = {
         r = r,
         render = Page.render,
@@ -21,8 +24,8 @@ function sailor.init(r,p)
         include = Page.include,
         write = function(_,...) r:write(...) end,
         print = function(_,...) r:puts(...) end,
-        POST = POST,
         GET = GET,
+        POST = POST,
         POSTMULTI = POSTMULTI,
         layout = conf.sailor.layout,
         title = conf.sailor.app_name,
