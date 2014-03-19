159b9f5ac56f955b30bad8dfdb012d703f21e2a8
Felipe Daragon <felipe@206labs.com>
2014-03-19 16:04:00 -0300

    Remove now unnecessary package.path line from index.lua files

--- src/sailor/demo-app/index.lua
+++ src/sailor/demo-app/index.lua
@@ -1,4 +1,2 @@
-local sailor_path = {{path}}
-package.path = package.path .. ';'..sailor_path..'/?.lua'
 require "sailor"
 sailor.launch()
--- test/dev-app/index.lua
+++ test/dev-app/index.lua
@@ -1,4 +1,2 @@
-local sailor_path = (debug.getinfo(1).source):match('^@?(.-)/index.lua$')..'/../../src'
-package.path = package.path .. ';'..sailor_path..'/?.lua'
 require "sailor"
 sailor.launch()
