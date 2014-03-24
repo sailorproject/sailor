6195dd3252dfd4fc987458e484bfc7ec707190d3
Etiene <dalcol@etiene.net>
2014-03-24 13:29:45 +0000

    Debug source returns nil on cgilua.

--- test/dev-app/index.lua
+++ test/dev-app/index.lua
@@ -1,3 +1,3 @@
-package.path = package.path .. ';'..(debug.getinfo(1).source):match('^@?(.-)/index.lua$')..'/../../src/?.lua'
+package.path = package.path .. ';'..((debug.getinfo(1).source):match('^@?(.-)/index.lua$') or '')..'/../../src/?.lua'
 require "sailor"
 sailor.launch()
