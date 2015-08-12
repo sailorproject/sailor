dc00f739351bf30091522ab20e3cee2d4547a4ca
Etiene Dalcol <dalcol@etiene.net>
2015-08-12 09:56:36 +0200

    Fixes dev test app not finding sailor dir

--- test/dev-app/index.lua
+++ test/dev-app/index.lua
@@ -1,4 +1,6 @@
-package.path =  ((debug.getinfo(1).source):match('^@?(.-)/index.lua$') or '')..'../../src/?.lua;'..package.path
+local base_dir = ((debug.getinfo(1).source):match('^@?(.-)index.lua$') or '')
+
+package.path =  base_dir..'../../src/?.lua;'..package.path
 
 local sailor = require "sailor"
 sailor.launch()
