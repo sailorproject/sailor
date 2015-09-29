92d5d579aff714c5ae8b5d9a65afbbe7118b6fe8
Felipe Daragon <felipe@206labs.com>
2015-09-28 22:00:19 -0300

    Improve latclient support in persistent environments

--- src/latclient.lua
+++ src/latclient.lua
@@ -1,7 +1,7 @@
 --[[
 Lua@Client 0.3.1
 Lua Pages Template Preprocessor Extension
-Copyright (c) 2014 Felipe Daragon
+Copyright (c) 2014-2015 Felipe Daragon
 
 License: MIT
 ]]
@@ -13,6 +13,11 @@ local vm = require("latclient."..conf.lua_at_client.vm)
 vm.js_url = conf.lua_at_client.vm_url or "./pub"
 
 function M.translate(src)
+  -- Reset for persistent environments (like Lighttpd)
+  vm.js_served = false
+  vm.modules_served = {}
+  
+  -- Starts processing Lua@Client tags
 	local get_both = function(s) return '<? '..s..' ?>'..vm.get_client_js(s) end
 	src = string.gsub(src,'<[?]lua@both%s*(.-)%s*[?]>',get_both) -- must come first
 	src = string.gsub(src,'<[?]lua@client%s*(.-)%s*[?]>',vm.get_client_js)
