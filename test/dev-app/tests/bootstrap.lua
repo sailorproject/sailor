0d37dfccc4aa2c97935cdc227c3e7d628535f8e3
Etiene Dalcol <dalcol@etiene.net>
2015-10-12 00:46:33 +0200

    test(bootstrap): Loading post fixtures on bootstrap
    Loading sample post data into db before tests

--- test/dev-app/tests/bootstrap.lua
+++ test/dev-app/tests/bootstrap.lua
@@ -1,8 +1,9 @@
 -- Test Bootstrap file
 -- This file will run before running tests
 sailor = require "sailor"
-
 local t = require "sailor.test"
 
 t.load_fixtures('user')
 t.load_fixtures('category')
+t.load_fixtures('post')
+--t.load_fixtures()
