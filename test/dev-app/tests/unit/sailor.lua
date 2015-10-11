2e15978e5d0915efe16ef88d2bf7e035b1f91083
Etiene Dalcol <dalcol@etiene.net>
2015-10-12 01:26:22 +0200

    test(sailor): Makes test compatible with luajit
    The order of passing trough the parameters table on sailor was differently than expected but there is no practical difference. The tests now take this into consideration.

--- test/dev-app/tests/unit/sailor.lua
+++ test/dev-app/tests/unit/sailor.lua
@@ -3,12 +3,11 @@ local conf = require "conf.conf"
 
 describe("Testing Sailor core functions", function()
   local base_path
-  setup(function()
- 
-  end)
+  local parms = {id = 5, name = 'a_test'}
 
   it("should create URLs accordingly without friendly urls", function()
     conf.sailor.friendly_urls = false
+    
 
     local url = sailor.make_url('test')
     assert.is_equal('?r=test',url)
@@ -16,24 +15,32 @@ describe("Testing Sailor core functions", function()
     url = sailor.make_url('test/etc')
     assert.is_equal('?r=test/etc',url)
 
-    url = sailor.make_url('test',{id = 5, name = 'a_test'})
-    assert.is_equal('?r=test&id=5&name=a_test',url)
+    url = sailor.make_url('test', parms)
+    local exp = '?r=test'
+    for k,v in pairs(parms) do exp = exp .. '&' .. k .. '=' .. v end
+    assert.is_equal(exp,url)
 
-    url = sailor.make_url('test/etc',{id = 5, name = 'a_test'})
-    assert.is_equal('?r=test/etc&id=5&name=a_test',url)
+    url = sailor.make_url('test/etc', parms)
+    exp = '?r=test/etc'
+    for k,v in pairs(parms) do exp = exp .. '&' .. k .. '=' .. v end
+    assert.is_equal(exp,url)
 
     base_path, sailor.base_path = sailor.base_path, '/sailor/test/dev-app'
     url = sailor.make_url('test/etc')
     assert.is_equal('/sailor/test/dev-app/?r=test/etc',url)
 
-    url = sailor.make_url('test',{id = 5, name = 'a_test'})
+    url = sailor.make_url('test', parms)
+    exp = '?r=test'
+    for k,v in pairs(parms) do exp = exp .. '&' .. k .. '=' .. v end
     assert.is_equal('/sailor/test/dev-app/?r=test&id=5&name=a_test',url)
 
-    url = sailor.make_url('test/etc',{id = 5, name = 'a_test'})
-    assert.is_equal('/sailor/test/dev-app/?r=test/etc&id=5&name=a_test',url)
+    url = sailor.make_url('test/etc')
+    assert.is_equal('/sailor/test/dev-app/?r=test/etc',url)
 
-    url = sailor.make_url('test/etc',{id = 5, name = 'a_test'})
-    assert.is_equal('/sailor/test/dev-app/?r=test/etc&id=5&name=a_test',url)
+    url = sailor.make_url('test/etc', parms)
+    exp = '/sailor/test/dev-app/?r=test/etc'
+    for k,v in pairs(parms) do exp = exp .. '&' .. k .. '=' .. v end
+    assert.is_equal(exp,url)
     base_path, sailor.base_path = sailor.base_path, base_path
   end)
 
@@ -46,24 +53,32 @@ describe("Testing Sailor core functions", function()
     url = sailor.make_url('test/etc')
     assert.is_equal('/test/etc',url)
 
-    url = sailor.make_url('test',{id = 5, name = 'a_test'})
-    assert.is_equal('/test/id/5/name/a_test',url)
+    url = sailor.make_url('test',parms)
+    local exp = '/test'
+    for k,v in pairs(parms) do exp = exp .. '/' .. k .. '/' .. v end
+    assert.is_equal(exp,url)
 
-    url = sailor.make_url('test/etc',{id = 5, name = 'a_test'})
-    assert.is_equal('/test/etc/id/5/name/a_test',url)
+    url = sailor.make_url('test/etc',parms)
+    local exp = '/test/etc'
+    for k,v in pairs(parms) do exp = exp .. '/' .. k .. '/' .. v end
+    assert.is_equal(exp,url)
 
     base_path, sailor.base_path = sailor.base_path, '/sailor/test/dev-app'
     url = sailor.make_url('test/etc')
     assert.is_equal('/sailor/test/dev-app/test/etc',url)
 
-    url = sailor.make_url('test',{id = 5, name = 'a_test'})
-    assert.is_equal('/sailor/test/dev-app/test/id/5/name/a_test',url)
+    url = sailor.make_url('test')
+    assert.is_equal('/sailor/test/dev-app/test',url)
 
-    url = sailor.make_url('test/etc',{id = 5, name = 'a_test'})
-    assert.is_equal('/sailor/test/dev-app/test/etc/id/5/name/a_test',url)
+    url = sailor.make_url('test',parms)
+    exp = '/sailor/test/dev-app/test'
+    for k,v in pairs(parms) do exp = exp .. '/' .. k .. '/' .. v end
+    assert.is_equal(exp,url)
 
-    url = sailor.make_url('test/etc',{id = 5, name = 'a_test'})
-    assert.is_equal('/sailor/test/dev-app/test/etc/id/5/name/a_test',url)
+    url = sailor.make_url('test/etc',parms)
+    exp = '/sailor/test/dev-app/test/etc'
+    for k,v in pairs(parms) do exp = exp .. '/' .. k .. '/' .. v end
+    assert.is_equal(exp,url)
     base_path, sailor.base_path = sailor.base_path, base_path
   end)
 end)
