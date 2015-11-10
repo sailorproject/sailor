2901897b45b02d52dbc444607e5eed36d8e3eee5
Etiene Dalcol <dalcol@etiene.net>
2015-11-10 09:08:01 +0100

    tests(openresty) Fix openresty path on travis

--- .travis/setup_servers.sh
+++ .travis/setup_servers.sh
@@ -7,11 +7,19 @@ OPENRESTY_VERSION="1.9.3.1"
 wget https://openresty.org/download/ngx_openresty-$OPENRESTY_VERSION.tar.gz
 tar xzvf ngx_openresty-$OPENRESTY_VERSION.tar.gz
 cd ngx_openresty-$OPENRESTY_VERSION/
-./configure --prefix="$TRAVIS_BUILD_DIR/install/nginx"
+
+if [ "$LUA" == "lua5.1" ]; then
+	./configure --prefix="$TRAVIS_BUILD_DIR/install/nginx" --with-lua51=$TRAVIS_BUILD_DIR/install/lua
+elif [ "$LUA" == "lua5.2" ]; then
+	./configure --prefix="$TRAVIS_BUILD_DIR/install/nginx"
+elif [ "$LUA" == "luajit" ]; then
+	./configure --prefix="$TRAVIS_BUILD_DIR/install/nginx" --with-luajit=$TRAVIS_BUILD_DIR/install/lua
+fi
+
 make
 make install
 
-export PATH=${PATH}:$HOME/.lua:$HOME/.local/bin:${TRAVIS_BUILD_DIR}/install/nginx/sbin
+export PATH=${PATH}:$HOME/.lua:$HOME/.local/bin:${TRAVIS_BUILD_DIR}/install/nginx/nginx/sbin
 
 cd ../
 rm -rf ngx_openresty-$OPENRESTY_VERSION
