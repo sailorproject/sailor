6581a87ec9b42ee35589c8a3321ad9502382584b
Etiene Dalcol <dalcol@etiene.net>
2015-11-10 08:48:36 +0100

    tests(openresty): Fixes setup for openresty on travis

--- .travis/setup_servers.sh
+++ .travis/setup_servers.sh
@@ -5,8 +5,8 @@
 OPENRESTY_VERSION="1.9.3.1"
 
 wget https://openresty.org/download/ngx_openresty-$OPENRESTY_VERSION.tar.gz
-tar xzvf ngx_openresty-OPENRESTY_VERSION.tar.gz
-cd ngx_openresty-OPENRESTY_VERSION/
+tar xzvf ngx_openresty-$OPENRESTY_VERSION.tar.gz
+cd ngx_openresty-$OPENRESTY_VERSION/
 ./configure
 make
 make install
@@ -14,7 +14,7 @@ make install
 export PATH=${PATH}:$HOME/.lua:$HOME/.local/bin:${TRAVIS_BUILD_DIR}/install/nginx/sbin
 
 cd ../
-rm -rf ngx_openresty-OPENRESTY_VERSION
+rm -rf ngx_openresty-$OPENRESTY_VERSION
 
 nginx -v 
 resty -v 
