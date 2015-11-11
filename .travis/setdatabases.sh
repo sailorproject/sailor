5fda2856ab392f4f1c3e10d61538bfa878be2b55
Etiene Dalcol <dalcol@etiene.net>
2015-11-11 11:12:31 +0100

    test(sqlite3): Adds sqlite3 to Travis CI tests

--- .travis/setdatabases.sh
+++ .travis/setdatabases.sh
@@ -1,10 +1,12 @@
 if [ "$DB_DRIVER" == "postgres" ]; then
-	pg_config
 	luarocks install luasql-postgres PGSQL_INCDIR=/usr/include/postgresql
 
 	psql -c 'create database sailor_test;' -U postgres
 	psql $DB_NAME < test/dev-app/sql/pgsql.sql
 
+elif [ "$DB_DRIVER" == "sqlite3" ]; then
+	luarocks install luasql-sqlite3
+	sqlite3 $TRAVIS_BUILD_DIR/sailor_test.db -init test/dev-app/sql/sqlite3.sql
 else
 	luarocks install luasql-mysql MYSQL_INCDIR=/usr/include/mysql
 
