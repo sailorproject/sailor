8d5c01b9a70119316ddc0f2f631c942569f3ee1e
Etiene Dalcol <dalcol@etiene.net>
2015-11-11 11:20:28 +0100

    tests(sqlite3): Fixing syntax on sqlite3 create db

--- .travis/setdatabases.sh
+++ .travis/setdatabases.sh
@@ -6,7 +6,7 @@ if [ "$DB_DRIVER" == "postgres" ]; then
 
 elif [ "$DB_DRIVER" == "sqlite3" ]; then
 	luarocks install luasql-sqlite3
-	sqlite3 $TRAVIS_BUILD_DIR/sailor_test.db -init test/dev-app/sql/sqlite3.sql
+	sqlite3 $TRAVIS_BUILD_DIR/sailor_test < test/dev-app/sql/sqlite3.sql
 else
 	luarocks install luasql-mysql MYSQL_INCDIR=/usr/include/mysql
 
