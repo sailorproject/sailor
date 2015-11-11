dd3de197335be587e07421f81d518e70b12e98c0
Etiene Dalcol <dalcol@etiene.net>
2015-11-11 11:30:23 +0100

    tests(openresty): Corrects script command and removes luasql install

--- .travis/setdatabases.sh
+++ .travis/setdatabases.sh
@@ -8,8 +8,9 @@ elif [ "$DB_DRIVER" == "sqlite3" ]; then
 	luarocks install luasql-sqlite3
 	sqlite3 $TRAVIS_BUILD_DIR/sailor_test < test/dev-app/sql/sqlite3.sql
 else
-	luarocks install luasql-mysql MYSQL_INCDIR=/usr/include/mysql
-
+	if [ ${SERVER} != "openresty" ]; then 
+		luarocks install luasql-mysql MYSQL_INCDIR=/usr/include/mysql
+	fi
 	mysql -e 'create database sailor_test;'
 	mysql $DB_NAME < test/dev-app/sql/mysql.sql
 fi
