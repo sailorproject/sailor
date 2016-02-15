7198e5f4d7ad6266f0834548f3306f7e0df301ba
Etiene Dalcol <dalcol@etiene.net>
2016-02-15 14:31:50 +0100

    chore(latclient): Remove latclient copy from sailor's repo

--- src/latclient/conf.lua
+++
@@ -1,8 +0,0 @@
-local conf = {
-	lua_at_client = {
-		vm = "starlight", -- starlight is default. Other options are moonshine, lua51js and luavmjs.
-		vm_url = nil -- allows to set a custom URL for the VM JavaScript files.
-	}
-}
-
-return conf
