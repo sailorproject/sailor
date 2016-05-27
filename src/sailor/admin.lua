715e29e0c907f527472896273a6caab8da4fa932
Nikhil R <rnikhil.2014@gmail.com>
2016-05-27 16:48:55 +0530

    Just checks for password for now

--- src/sailor/admin.lua
+++ src/sailor/admin.lua
@@ -2,11 +2,28 @@ local M ={}
 function M.gen()
 	local code=[[
 
+<?lua
+local conf = require "conf.conf"
+
+print('conf.sailor.admin_password')
+
+if next(page.POST) then
+	if page.POST.password then
+		if (conf.sailor.admin_password == page.POST.password) then
+			?><p>login success</p><?lua
+		end
+	end
+end
+
+	
+?>
+	
 	<h4>Admin Center</h4>
 <form method="post">
-	<input type=text placeholder="Enter Password set in conf file" name="password"/>
+	<input type=text placeholder="Enter Password set in config file" name="password"/>
 	<input type="submit" />
 </form>
+
 
 	]]
 	return code
