3e8602314a9a7d0f6d10a1713ebc46867f6e7c6a
Nikhil R <rnikhil.2014@gmail.com>
2016-06-18 11:06:40 +0530

    edit(conf): Make admin centre and autogen default to false

--- src/sailor/blank-app/conf/conf.lua
+++ src/sailor/blank-app/conf/conf.lua
@@ -14,8 +14,8 @@ local conf = {
 		max_upload = 1024 * 1024, 
 		environment = "development",  -- this will use db configuration named development
 		hide_stack_trace = false, -- false recommended for development, true recommended for production
-		enable_admin=true, -- set true only in development. Set password below too.
-		admin_password="test"
+		enable_admin=false, -- set true only in development. Set password below too.
+		admin_password=""
 	},
 
 	db = {
