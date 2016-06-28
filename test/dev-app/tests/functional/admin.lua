5005b4cbcf197041a887f9c64240dcf6ee93869e
Nikhil R <rnikhil.2014@gmail.com>
2016-06-28 15:33:01 +0530

    tests(admin): Tests for autogen
    Add files to dev-app for testing, modify previous tests for autogen

--- test/dev-app/tests/functional/admin.lua
+++ test/dev-app/tests/functional/admin.lua
@@ -8,34 +8,56 @@ describe("Testing Admin", function ()
 	it("Admin Page should not open", function()
  	conf.sailor.enable_admin = false
     local res = test.request('admin')
-    assert.truthy(res.body:match('Error'))
+    assert.truthy(res.body:match('Enable Admin first in the config file'))
   end)
 
-	it("Admin page should open", function()
-  	conf.sailor.enable_admin = true
-    local res = test.request('admin')
-    assert.same(200,res.status)
-    assert.truthy(res.body:match('Admin Center'))
-  end)
-
-	it("Should Not login to admin", function()
-  	conf.sailor.enable_admin = true
-    local res = test.request('admin',{post={password = "randomtstaa"}})
-    assert.falsy(res.body:match('Login Success.'))
-  end)
-
-	it("Should Not login to admin", function()
-  	conf.sailor.enable_admin = true
-  	conf.sailor.admin_password = "test"
+  it("Admin page should open", function()
+    conf.sailor.enable_admin = true
+    conf.sailor.admin_password = "test"
     local res = test.request('admin',{post={password = "test"}})
-    assert.falsy(res.body:match('Login Success.'))
+    assert.is_true(res:redirected('admin/dashboard'))
+    assert.same(200,res.status)
+    assert.truthy(res.body:match('Generate CRUD'))
+    assert.truthy(res.body:match('Config Editor'))
   end)
 
+  it("should not generate model", function()
+    local res = test.request('admin',{post={table_name ='asdasd'}})
+    assert.falsy(res.body:match('Model generated with success'))
+  end)
 
+  it("should generate model", function()
+    local path = 'models/category.lua'
+    os.remove(path)
+    local res = test.request('admin',{post={table_name ='category'}})
+    assert.truthy(res.body:match('Model generated with success'))
+    assert.truthy(lfs.attributes(path))
+  end)
 
+  it("should not generate CRUD", function()
+    local res = test.request('admin',{post={model_name ='asdasd'}})
+    assert.falsy(res.body:match('CRUD generated with success'))
+  end)
 
+  it("should generate CRUD", function()
+    local paths = {
+      'controllers/category.lua',
+      'views/category/create.lp',
+      'views/category/index.lp',
+      'views/category/update.lp',
+      'views/category/view.lp'
+    }
+    for _,f in ipairs(paths) do
+      os.remove(f)
+    end
 
-
+    local res = test.request('admin',{post={model_name ='category'}})
+    assert.truthy(res.body:match('CRUD generated with success'))
+    for _,f in ipairs(paths) do
+       assert.truthy(lfs.attributes(f))
+    end
+  end)
+	
 
 end)
 
