3a321990cc44f29dc1779fef03427ced56bbc9ce
Nikhil R <rnikhil.2014@gmail.com>
2016-08-09 16:36:31 +0530

    feat(elasticsearch): Support for parameters to be passed

--- src/sailor/db/elasticsearch.lua
+++ src/sailor/db/elasticsearch.lua
@@ -6,14 +6,12 @@
 -- http://sailorproject.org
 -------------------------------------------------------------------------------------------
 
-
-
 local elasticsearch = require "elasticsearch"
 local main_conf = require "conf.conf"
 local elastic_conf = main_conf.search[main_conf.sailor.search_engine]
 local es = {}
 
-	local client = elasticsearch.client{
+local client = elasticsearch.client{
 	hosts=elastic_conf.hosts,
 	params = elastic_conf.params
 }
@@ -24,22 +22,28 @@ function es.getinfo()
 		return err
 	else
 		return data , err
-
 	end
 end
 
-function es.index(typeq, idq, body, params)
-	local t = {}
-	-- for k, v in params do client:index{v} end
-
-	for k,v in pairs(params) do  t[v] = true end
+function es.index(type, id, body, params)
+	params = params or {} 
 	local data, err = client:index{
-		index = elastic_conf.index,
-		type = typeq,
-		id = idq,
-		body = body,
-		
 
+		index = elastic_conf.index,
+		type = type,
+		id = id,
+		body = body,
+		routing = params.routing,
+		op_type = params.op_type,
+		consistency = params.consistency,
+		parent = params.parent,
+		refresh = params.refresh,
+		percolate = params.percolate,
+		replication = params.replication,
+		timeout = params.timeout,
+		version = params.version,
+		version_type = params.version_type
+	
 	}
 	if data == nil then
 		return err
@@ -49,11 +53,24 @@ function es.index(typeq, idq, body, params)
 	end
 end
 
-function es.get(typeq, idq)
+function es.get(type, id, params)
+	params = params or {}
 	local data, err = client:get{
+
 	  	index = elastic_conf.index,
 	  	type = typeq,
-	  	id = idq
+	  	id = idq,
+	  	ignore_missing = params.ignore_missing,
+	  	fields = params.fields,
+	  	parent = params.parent,
+	  	preference = params.preference,
+	  	realtime = params.realtime,
+	  	refresh = params.refresh,
+	  	routing = params.routing,
+	  	_source = params._source,
+	  	_source_exclude = params._source_exclude,
+	  	_source_include = params._source_include
+
 	}
 	if data == nil then
 		return err
@@ -62,11 +79,41 @@ function es.get(typeq, idq)
 	end
 end
 
-function es.search(typeq, query)
+function es.search(type, query, params)
+	params = params or {}
 	local data, err = client:search{
   		index = elastic_conf.index,
-  		type = typeq,
-  		q = query
+  		type = type,
+  		q = query,
+  		_source = params._source,
+	  	_source_exclude = params._source_exclude,
+	  	_source_include = params._source_include,
+	  	analyzer = params.analyzer,
+	  	analyze_wildcard = params.analyze_wildcard,
+	  	default_operator = params.default_operator,
+	  	df = params.df,
+	  	explain = params.explain,
+	  	fields = params.fields,
+	  	from = params.from,
+	  	ignore_indices = params.ignore_indices,
+	  	indices_boost = params.indices_boost,
+	  	lenient = params.lenient,
+	  	lowercase_expanded_terms = params.lowercase_expanded_terms,
+	  	preference = params.preference,
+	  	routing = params.routing,
+	  	scroll = params.scroll,
+	  	search_type = params.search_type,
+	  	size = params.size,
+	  	sort = params.sort,
+	  	source = params.source,
+	  	stats = params.stats,
+	  	suggest_field = params.suggest_field,
+	  	suggest_mode = params.suggest_mode,
+	  	suggest_size = params.suggest_size,
+	  	suggest_text = params.suggest_text,
+	  	timeout = params.timeout,
+	  	version = params.version
+
 		}
 	if data == nil then
 		return err
@@ -76,30 +123,21 @@ function es.search(typeq, query)
 	end
 end
 
-function es.searchbody(typeq, body)
-	local data, err = client:search{
-	  index = elastic_conf.index,
-	  type = typeq,
-	  body = {
-	    query = {
-	      match = body
-	    }
-	  }
-}
-if data == nil then
-		return err
-	else
-		return data,err
 
-	end
-end
-
-function es.delete(typeq, idq)
+function es.delete(type, id, params)
+	params = params or {}
 	local data, err = client:delete{
-	  index = elastic_conf.index,
-	  type = typeq,
-	  id = idq
-}
+	  	index = elastic_conf.index,
+	  	type = type,
+	  	id = id,
+	  	consistency = params.consistency,
+		parent = params.parent,
+		refresh = params.refresh,
+		replication = params.replication,
+		routing = params.routing,
+		timeout = params.timeout,
+		version_type = params.version_type
+	}
 	if data == nil then
 		return err
 	else
@@ -110,12 +148,25 @@ function es.delete(typeq, idq)
 end
 
 
-function es.update(typeq, idq, body)
+function es.update(type, id, body, params)
+	params = params or {}
 	local data, err = client:update{
 		index = elastic_conf.index,
-		type = typeq,
-		id = idq,
-			body = {
+		type = type,
+		id = id,
+		consistency = params.consistency,
+		fields = params.fields,
+		lang = params.lang,
+		parent = params.parent,
+		percolate = params.percolate,
+		refresh = params.refresh,
+		replication = params.replication,
+		retry_on_conflict = params.retry_on_conflict,
+		routing = params.routing,
+		script = params.script,
+		timeout = params.timeout,
+		version_type = params.version_type,
+		body = {
 			doc = body
   }
 }
